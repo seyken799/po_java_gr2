ce9341e78f8d25a0ad1b3292862cea8cbbf9829a
seyken799 <73948604+seyken799@users.noreply.github.com>
2021-10-19 15:06:15 +0200

    Add files via upload

---
+++ lab01.java
@@ -0,0 +1,62 @@
+package lab00;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Zad1
+
+        System.out.println(31 + 29 + 31);
+        //Zad2
+        int suma = 0;
+        for (int i = 1; i <= 10; i++) {
+            suma += i;
+
+        }
+        System.out.println(suma);
+        //Zad3
+        int iloczyn = 1;
+        for (int i = 1; i <= 10; i++) {
+            iloczyn *= i;
+
+        }
+        System.out.println(iloczyn);
+        //Zad4
+        float stanKonta = 1000;
+        for (int i = 1; i <= 3; i++) {
+            stanKonta += stanKonta * 0.06;
+            System.out.println("Saldo po " + i + " wynosi " + stanKonta);
+
+
+        }
+        //Zadanie 5
+
+        System.out.println("______\n|JAVA|\n------");
+
+
+        //Zadanie 6
+        System.out.println("    /////   ");
+        System.out.println("   +-----+");
+        System.out.println("  (| o o |)");
+        System.out.println("    | ˆ |");
+        System.out.println("   | ʻ-ʼ |");
+        System.out.println("   +-----+");
+
+        //zadanie 11
+        System.out.println("Wielkie to szczęście\n" +
+                "nie wiedzieć dokładnie,\n" +
+                "na jakim świecie się żyje.\n" +
+                "\n" +
+                "Trzeba by było\n" +
+                "istnieć bardzo długo,\n" +
+                "stanowczo dłużej\n" +
+                "niż istnieje on.\n" +
+                "\n" +
+                "Choćby dla porównania\n" +
+                "poznać inne światy.");
+
+
+
+
+
+
+
