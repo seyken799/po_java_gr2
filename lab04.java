5c98cbad3bef058f0f6b80a1032b9b737fdd94af
seyken799 <73948604+seyken799@users.noreply.github.com>
2021-11-02 22:37:50 +0100

    Add files via upload

---
+++ lab04.java
@@ -0,0 +1,49 @@
+public class lab04 {
+    //a
+//    public static int countChar(String str, char c){
+//        char[] chars= str.toCharArray();
+//        int count =0;
+//        for(char a: chars){
+//            if(a==c){
+//                count++;
+//            }
+//        }
+//        return count;
+//    }
+//    public static void main(String[] args) {
+//        System.out.println("Ile jest liter c:" + countChar("dkjasckjxzbnkzxcbcvbxzvabsv", 'c'));
+//    }
+    //b
+    //c
+//    public static String middle(String str){
+//        int position ;
+//        int length;
+//        if(str.length()%2==0){
+//            position = str.length()/2-1;
+//            length = 2;
+//        }
+//        else{
+//            position = str.length()/2;
+//            length=1;
+//        }
+//        return str.substring(position, position + length);
+//    }
+//    public static void main(String[] args) {
+//
+//        System.out.println("Srodkowa : " + middle("middle"));
+//    }
+    //d
+//    public static String repeat(String str, int n) {
+//        StringBuffer sb =new StringBuffer(str.length()*n);
+//        for(int i=0; i<n; i++){
+//            sb.append(str);
+//        }
+//        return sb.toString();
+//    }
+//    public static void main(String[] args) {
+//
+//        System.out.println("Powtorz: " + repeat("abc ",3));
+//    }
+    //e
+
+}
